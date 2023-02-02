1ec29b8864c396c0cb740d2484b7638b9238443d
Farrukh Sultani <farrukh.sultani@gmail.com>
2023-02-01 22:34:19 -0800

    cleaned the folder

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "8ead2b3362bfe430b756fadde4b5d70a"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "202e1eef2ca540d5a6cc3e766c835277"
